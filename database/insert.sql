d87051e6741bf16f976058abbcbd24db593a9798
alessiorubicini <alessiorubicini05@gmail.com>
2023-12-20 16:54:00 +0100

    Implementazione DB + dati di debug

---
+++ database/insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO Content (title, description, date, pointId, statusId)
+VALUES
+  ('Festa del Torrone', 'Early morning beauty', '2023-01-15', 1, 2),
+  ('Festone al campus', 'Golden hour at the pier', '2023-02-15', 2, 2);
