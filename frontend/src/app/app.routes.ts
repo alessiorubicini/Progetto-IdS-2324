219760defbca6ffc4ad0210594bb1df942067a22
Vittoria61 <vittoria.forlani@gmail.com>
2023-12-30 19:07:49 +0100

    Contacts component implemented

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,11 +3,14 @@ import { LoginComponent } from "./login/login.component"
 import { SignupComponent } from "./signup/signup.component"
 import { HomeComponent } from "./home/home.component"
 import { CityDetailComponent } from "./city-detail/city-detail.component"
+import { ContactsComponent } from "./contacts/contacts.component"
 
 export const routes: Routes  = [
 	{ path: "", redirectTo: "home", pathMatch: "full" },
 	{ path: "home", component: HomeComponent },
 	{ path: "login", component: LoginComponent },
 	{ path: "signup", component: SignupComponent },
-	{ path: "city/:id", component: CityDetailComponent }
+	{ path: "city/:id", component: CityDetailComponent },
+	{ path: "contacts", component: ContactsComponent }
+	
 ]
